9e62a6d4675f9522e7360b2192a6aeca72aaae06
cataliniimolnar <31444220+cataliniimolnar@users.noreply.github.com>
2017-12-26 15:43:23 +0200

    26Dec_343
    V1

---
+++ assets/js/p5/sketch.js
@@ -0,0 +1,51 @@
+let snow = [];
+let gravity;
+let zOff = 0;
+
+
+let spritesheet;
+let textures = [];
+
+function preload(){
+    spritesheet = loadImage('https://alca.tv/static/f32.png');
+}
+
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    gravity = createVector(0, 0.03);
+    for (let x = 0; x < spritesheet.width; x+=32) {
+        for (let y = 0; y < spritesheet.height; y+=32) {
+            let img = spritesheet.get(x, y, 32, 32);
+            textures.push(img);
+            image(img, x, y);
+        }
+    }
+    
+    let xr = random(450, 600);
+    for (let i = 0; i < xr; i++) {
+        let x = random(width);
+        let y = random(height);
+        let design = random(textures);
+        snow.push(new SnowFlake(x, y, design));
+    }
+    
+    
+}
+
+
+function draw(){
+    background(34);
+    zOff += 0.01;
+    for (flake of snow) {
+        let xOff = flake.pos.x / width;
+        let yOff = flake.pos.y / height;
+        let wAngle = noise(xOff, yOff, zOff) * TWO_PI;
+        let wind = p5.Vector.fromAngle(wAngle);
+        wind.mult(0.02);
+        flake.applyForce(gravity);
+        flake.applyForce(wind);
+        flake.update();
+        flake.render();
+    }
+}
